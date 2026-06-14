143029aab48f99b7590eb4a27ed8505bf8c1ab45
theknight666 <yupppitsmukul@gmail.com>
2026-06-15 04:55:35 +0530

    Revert react-snap to fix Vercel build

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { createRoot, hydrateRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "@/index.css";
 import App from "@/App";
 
-const rootElement = document.getElementById("root");
-
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(
-    rootElement,
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  const root = createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
